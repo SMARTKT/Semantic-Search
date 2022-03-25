b8347cfd77234c1067cea179087a7e3dde3be38e
Dewang Modi <mailtodewang@rediffmail.com>
2022-03-25 14:56:36 +0530

    Added the tool's code

---
+++ sample_projects/inherit/build/CMakeFiles/inherit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inherit.dir/src/inherit.cpp.o"
+  "inherit.pdb"
+  "inherit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inherit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
